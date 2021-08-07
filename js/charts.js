dcf4c3b7fa05e363e9024f92ba610fd46e1c43f6
Juan Nicolas Serrano <83378141+nicoserrano@users.noreply.github.com>
2021-08-06 22:24:46 -0400

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -135,11 +135,11 @@ function buildCharts(sample) {
       gauge: {
         axis: {range: [0,10]},
         steps: [
-          {range: [0,2], color:"#98ee00"},
-          {range: [2,4], color:"#d4ee00"},
-          {range: [4,6], color:"#eecc00"},
-          {range: [6,8], color:"#ee9c00"},
-          {range: [8,10], color:"#ea822c"}
+          {range: [0,2], color:"#ea2c2c"},
+          {range: [2,4], color:"#ea822c"},
+          {range: [4,6], color:"#ee9c00"},
+          {range: [6,8], color:"#eecc00"},
+          {range: [8,10], color:"#d4ee00"}
         ]
       }
     };
